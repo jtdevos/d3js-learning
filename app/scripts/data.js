e87efb5ff5d0bec2021340b0b24524e4d277bf70
nobodyman <jimdevos@tdar-imac5k.mobile.asu.edu>
2016-05-06 16:10:00 -0700

    coding a chart, manually

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -3,16 +3,4 @@ $(function() {
     console.log("hello from data");
     var data = [4, 8, 16, 23, 42];
 
-    var x = d3.scale.linear()
-        .domain([0, d3.max(data)])
-        .range([0, 420]);
-
-    var chart = d3.select(".chart");
-    var bar = chart.selectAll("div");
-    var barUpdate = bar.data(data);
-    var barEnter = barUpdate.enter().append("div");
-
-    barEnter.style("width", function(d) {return x(d) + "px";});
-    barEnter.text(function(d) {return d ;});
-
 });
