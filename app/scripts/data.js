7c65e3abbe0e9aa9fc4f71a215046a0f21c67ad2
nobodyman <jimdevos@tdar-imac5k.mobile.asu.edu>
2016-05-06 15:34:44 -0700

    lesson 1

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -1,7 +1,18 @@
-//misc data sources
 
-(function(){
-    "use strict";
+$(function() {
+    console.log("hello from data");
+    var data = [4, 8, 16, 23, 42];
 
-    console.log("data.js here");
-})();
+    var x = d3.scale.linear()
+        .domain([0, d3.max(data)])
+        .range([0, 420]);
+
+    var chart = d3.select(".chart");
+    var bar = chart.selectAll("div");
+    var barUpdate = bar.data(data);
+    var barEnter = barUpdate.enter().append("div");
+
+    barEnter.style("width", function(d) {return x(d) + "px";});
+    barEnter.text(function(d) {return d ;});
+
+});
