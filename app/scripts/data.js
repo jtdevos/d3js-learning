af3e99d0e943ee39d514f666e160ac8f6ec661db
nobodyman <jimdevos@tdar-imac5k.mobile.asu.edu>
2016-05-09 13:58:59 -0700

    end of lesson 3

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -1,41 +1,65 @@
 
 $(function() {
-    console.log("hello from data");
-    var width = 420;
-    var barHeight = 20;
-
-    var x = d3.scale.linear()
-        .range([0, width]);
-
-    var chart = d3.select(".chart")
-        .attr("width", width);
-
-
-    d3.tsv("/scripts/data.tsv", type, function(error, data) {
-        x.domain([0, d3.max(data, function(d) { return d.value; })]);
-
-        chart.attr("height", barHeight * data.length);
-
-        var bar = chart.selectAll("g")
-            .data(data)
-            .enter().append("g")
-            .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-
-        bar.append("rect")
-            .attr("width", function(d) { return x(d.value); })
-            .attr("height", barHeight - 1);
-
-        bar.append("text")
-            .attr("x", function(d) { return x(d.value) - 3; })
-            .attr("y", barHeight / 2)
-            .attr("dy", ".35em")
-            .text(function(d) { return d.value; });
-    });
-
     function type(d) {
         d.value = +d.value; // coerce to number
         return d;
     }
 
+var margin = {top: 20, right: 30, bottom: 30, left: 40},
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
+
+var x = d3.scale.ordinal()
+    .rangeRoundBands([0, width], .1);
+
+var y = d3.scale.linear()
+    .range([height, 0]);
+
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+
+var yAxis = d3.svg.axis()
+    .scale(y)
+    .orient("left");
+
+var chart = d3.select(".chart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+d3.tsv("/scripts/data.tsv", type, function(error, data) {
+  x.domain(data.map(function(d) { return d.name; }));
+  y.domain([0, d3.max(data, function(d) { return d.value; })]);
+
+  chart.append("g")
+      .attr("class", "x axis")
+      .attr("transform", "translate(0," + height + ")")
+      .call(xAxis);
+
+  chart.append("g")
+      .attr("class", "y axis")
+      .call(yAxis);
+
+  chart.selectAll(".bar")
+      .data(data)
+    .enter().append("rect")
+      .attr("class", "bar")
+      .attr("x", function(d) { return x(d.name); })
+      .attr("y", function(d) { return y(d.value); })
+      .attr("height", function(d) { return height - y(d.value); })
+      .attr("width", x.rangeBand());
+
+  chart.append("g")
+    .attr("class", "y axis")
+    .call(yAxis)
+  .append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y", 6)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Frequency");
+});
 
 });
