d48d6dfcc8eb01bb95d779c332c97b46814b67cd
nobodyman <jimdevos@tdar-imac5k.mobile.asu.edu>
2016-05-09 15:41:07 -0700

    done with circles tutorial

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -1,65 +1,18 @@
 
 $(function() {
-    function type(d) {
-        d.value = +d.value; // coerce to number
-        return d;
-    }
+    "use strict";
 
-var margin = {top: 20, right: 30, bottom: 30, left: 40},
-    width = 960 - margin.left - margin.right,
-    height = 500 - margin.top - margin.bottom;
+    var svg = d3.select("svg");
+    var circle = svg.selectAll("circle")
+    .data([32, 57, 293], function(d) { return d; });
 
-var x = d3.scale.ordinal()
-    .rangeRoundBands([0, width], .1);
+circle.enter().append("circle")
+    .attr("cy", 60)
+    .attr("cx", function(d, i) { return i * 100 + 30; })
+    .attr("r", function(d) { return Math.sqrt(d); });
 
-var y = d3.scale.linear()
-    .range([height, 0]);
+circle.exit().remove();
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
-
-var chart = d3.select(".chart")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom)
-  .append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-d3.tsv("/scripts/data.tsv", type, function(error, data) {
-  x.domain(data.map(function(d) { return d.name; }));
-  y.domain([0, d3.max(data, function(d) { return d.value; })]);
-
-  chart.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
-
-  chart.append("g")
-      .attr("class", "y axis")
-      .call(yAxis);
-
-  chart.selectAll(".bar")
-      .data(data)
-    .enter().append("rect")
-      .attr("class", "bar")
-      .attr("x", function(d) { return x(d.name); })
-      .attr("y", function(d) { return y(d.value); })
-      .attr("height", function(d) { return height - y(d.value); })
-      .attr("width", x.rangeBand());
-
-  chart.append("g")
-    .attr("class", "y axis")
-    .call(yAxis)
-  .append("text")
-    .attr("transform", "rotate(-90)")
-    .attr("y", 6)
-    .attr("dy", ".71em")
-    .style("text-anchor", "end")
-    .text("Frequency");
-});
 
 });
