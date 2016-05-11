d89c9134ba6a76fd289017b719687fcf02390502
Jim deVos <jim.devos@gmail.com>
2016-05-10 23:15:44 -0700

    finally figured out update vs. enter

--- app/scripts/data.js
+++ app/scripts/data.js
@@ -1,18 +1,52 @@
 
 $(function() {
     "use strict";
-
+    
+    
+    var data = [];
     var svg = d3.select("svg");
-    var circle = svg.selectAll("circle")
-    .data([32, 57, 293], function(d) { return d; });
+    var width = $("svg").width() + 0;
+    
+    function getid() {
+        return (new Date()).getTime();
+    }
+    
+    
+    function update() {
+        console.log("data is now %o", data);
+        var circle = svg.selectAll("circle")
+            .data(data, function(d) { return d.id; });
+        
 
-circle.enter().append("circle")
-    .attr("cy", 60)
-    .attr("cx", function(d, i) { return i * 100 + 30; })
-    .attr("r", function(d) { return Math.sqrt(d); });
+        circle.enter().append("circle")
+            .attr("cy", 60)
+            .attr("cx", function(d, i) { return 50 + (i * ((width-100) / data.length)); })
+            .attr("r", function(d) { return Math.sqrt(d.value); })
+            .attr("fill", "none")
+            .attr("stroke-width", "1")
+            .attr('stroke', 'gray');
+        
+        circle
+            .transition()
+            .duration(800)
+            .attr("cx", function(d, i) { return 50 + (i * ((width-100) / data.length)); })
 
-circle.exit().remove();
+            
+        circle.exit().remove();        
+    }
 
-
-
+    $('#btnAddItem').click(function() {
+        var item = {id: getid(), value: Math.round(Math.random() * 500)};
+        data.push(item);
+        update();
+    });
+    
+    $('#btnRemoveItem').click(function() {
+        data.pop();
+        update();
+        
+    })
+    
+    update();
+    
 });
